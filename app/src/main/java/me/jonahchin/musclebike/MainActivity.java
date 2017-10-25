d36f04400bd9a3e0322f1e1ad8d72d2bbd9f58ef
Jonah Chin <jonahchin7@gmail.com>
2017-10-25 02:19:48 -0400

    Added initial firebase functionality

--- app/src/main/java/me/jonahchin/musclebike/MainActivity.java
+++ app/src/main/java/me/jonahchin/musclebike/MainActivity.java
@@ -1,20 +1,27 @@
 package me.jonahchin.musclebike;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RideFragment rideFragment = new RideFragment();
         FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().add(R.id.primary_container, rideFragment).commit();
+        RideFragment rideFragment = (RideFragment) fm.findFragmentById(R.id.primary_container);
 
+        if(rideFragment == null) {
+            rideFragment = new RideFragment();
+            fm.beginTransaction()
+                    .add(R.id.primary_container, rideFragment)
+                    .commit();
+        }
     }
+
+
 }
--- app/src/main/java/me/jonahchin/musclebike/RideFragment.java
+++ app/src/main/java/me/jonahchin/musclebike/RideFragment.java
@@ -3,9 +3,19 @@ package me.jonahchin.musclebike;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by jonahchin on 2017-10-16.
@@ -13,13 +23,46 @@ import android.view.ViewGroup;
  */
 
 public class RideFragment extends Fragment {
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mForceRef = mRootRef.child("force");
+
+    EditText mForceData;
+    Button mUpdateButton;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ride, container, false);
-
-
+        mForceData = view.findViewById(R.id.force_data);
+        mUpdateButton = view.findViewById(R.id.update_button);
+        setListeners();
 
         return view;
+    }
+
+    private void setListeners() {
+        mForceRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String force = dataSnapshot.getValue(String.class);
+                mForceData.setText(force);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mForceRef.setValue(mForceData.getText().toString());
+            }
+        });
+
+
+
+
     }
 }
