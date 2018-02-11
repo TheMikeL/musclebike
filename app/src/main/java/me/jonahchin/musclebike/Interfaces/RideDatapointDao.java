5a4e56cc0d977b9b4ff04c47646bdc7d28829d41
Jonah Chin <jonahchin7@gmail.com>
2018-02-10 22:23:07 -0500

    SQL completely implemented + lists/stats adjusted

---
+++ app/src/main/java/me/jonahchin/musclebike/Interfaces/RideDatapointDao.java
@@ -0,0 +1,8 @@
+package me.jonahchin.musclebike.Interfaces;
+
+/**
+ * Created by jonahchin on 2018-02-10.
+ */
+
+public interface RideDatapointDao {
+}
---
+++ app/src/main/java/me/jonahchin/musclebike/Utility/StringUtil.java
@@ -0,0 +1,8 @@
+package me.jonahchin.musclebike.Utility;
+
+/**
+ * Created by jonahchin on 2018-02-10.
+ */
+
+public class StringUtil {
+}
